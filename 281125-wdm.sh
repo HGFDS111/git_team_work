6af3f7d65cea921aa8b1bee54dd5e5180b2d0c69
Ekaterina <e406096@gmail.com>
2026-03-17 13:41:48 +0300

    add issue3

--- 281125-wdm.sh
+++ 281125-wdm.sh
@@ -2,3 +2,7 @@
 read -p "Введите исходную директорию: " source_directory
 read -p "Введите целевую директорию: " target_directory
 read -p "Введите расширение файлов для копирования (без точки): " file_extension  
+if [ ! -d "$source_directory" ]; then
+echo "Директория '$source_directory' не существует или недоступна."
+exit 1
+fi
